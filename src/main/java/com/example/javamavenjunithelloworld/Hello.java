d8a90a1803a2a4705607c99bd9243c704a468b15
arditiya <made.arditiya@mitrais.com>
2016-12-14 14:24:12 +0800

    Update Hello.java

--- src/main/java/com/example/javamavenjunithelloworld/Hello.java
+++ src/main/java/com/example/javamavenjunithelloworld/Hello.java
@@ -32,7 +32,7 @@ public class Hello {
      */
     public void sayHello(PrintStream printer) {
         for (short i = 0; i < times; i++) {
-            printer.print(HELLO + "\n");
+            printer.print(HELLO);
         }
     }
 
